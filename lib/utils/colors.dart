33ebffcb2c776c46679d5e20706698fd84f8931e
OttawaSTEM <ottawastem@gmail.com>
2022-02-09 17:25:16 -0500

    add assets

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static final Color textColor = Colors.orange.shade50;
+  static final Color mainColor = Colors.lightBlue.shade100;
+  static final Color iconColor1 = Colors.amber.shade300;
+  static final Color iconColor2 = Colors.amber.shade700;
+  static final Color paraColor = Colors.grey.shade600;
+  static final Color buttonBackgroundColor = Colors.grey.shade50;
+  static final Color signColor = Colors.grey.shade500;
+  static final Color titleColor = Colors.grey.shade800;
+  static final Color mainBlackColor = Colors.grey.shade900;
+  static final Color yellowColor = Colors.yellow.shade600;
+}
